3b69603f434a61d77b09d8e9c6cde46315a7a861
Marcin Mackowiak <thaon12@gmail.com>
2026-01-25 21:51:47 -0800

    Added engagement tables schema and seed entries, also updated core sc… (#72)
    * Added engagement tables schema and seed entries, also updated core schema to use datetime instead of timestamp
    
    * Added favorites table to schema.sql

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,3 +1,5 @@
+-- CORE --
+
 INSERT INTO users (email, password_hash, role)
 VALUES
     ('shelter1@pawmatch.test', 'dev_hash_1', 'shelter_admin'),
@@ -14,3 +16,40 @@ INSERT INTO pets (shelter_id, name, species, breed, age_years, sex, size, descri
 SELECT s.id, 'Milo', 'Dog', 'Lab Mix', 3, 'M', 'Medium', 'Friendly and energetic.', 'available'
 FROM shelters s
 WHERE s.name = 'Happy Tails Shelter';
+
+-- ENGAGEMENT --
+INSERT INTO shelter_posts (shelter_id, type, title, body, published_at)
+SELECT s.id, 'SHELTER_POST', 'Adoption Event This Weekend', 'Join us Saturday from 12–4 PM!', CURRENT_TIMESTAMP 
+FROM shelters s
+WHERE s.name = 'Happy Tails Shelter';
+
+INSERT INTO shelter_follows (user_id, shelter_id)
+SELECT u.id, s.id
+FROM users u
+JOIN shelters s
+	ON s.name = 'Happy Tails Shelter'
+WHERE u.email = 'adopter1@pawmatch.test';
+
+INSERT INTO favorites (user_id, pet_id)
+SELECT u.id, p.id
+FROM users u
+JOIN pets p
+	ON p.name = 'Milo'
+WHERE u.email = 'adopter1@pawmatch.test';
+
+INSERT INTO feed_events (event_type, shelter_id, post_id)
+SELECT type, shelter_id, id
+FROM shelter_posts sp
+WHERE sp.title = 'Adoption Event This Weekend';
+
+INSERT INTO feed_events (event_type, shelter_id, pet_id, payload)
+SELECT 'PET_CREATED', s.id, p.id, JSON_OBJECT('pet_name', p.name)
+FROM shelters s
+JOIN pets p 
+	ON p.shelter_id = s.id
+WHERE s.name = 'Happy Tails Shelter';
+
+INSERT INTO email_notifications (user_id)
+SELECT u.id
+FROM users u
+WHERE u.email = 'adopter1@pawmatch.test';
