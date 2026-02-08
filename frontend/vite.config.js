689735f3076f2b89517d44c0921ed8509a5dd89f
Giovanita Bell <129814598+gioviebell@users.noreply.github.com>
2026-02-08 03:31:05 -0600

    Added drop down for shelter selection until account creation is complete. Also added vite proxy to connect frontend and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Allows the frontend to send requests to the backend without middleware 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/pets': 'http://localhost:4516',
+      '/shelters': 'http://localhost:4516',
+      '/users': 'http://localhost:4516',
+      '/api': 'http://localhost:4516'
+    }
+  }
 })
